bbf1df4b9bee29bb67cc767a2b9559cc9197f6d6
SagarMhzn <maharzansagar4u@gmail.com>
2023-01-19 06:35:06 +0545

    crud

--- src/Unit2/JDBCDemo.java
+++ src/Unit2/JDBCDemo.java
@@ -19,11 +19,46 @@ public class JDBCDemo {
             
             Statement st =  conn.createStatement();
             
-            //query for creating table
-            String tblQuery = "create table tbl_std(id int primary key,name varchar(50),password varchar(50)"
-                    + ",repassword varchar(50), gender varchar(50),course varchar(50))";
+//            //query for creating table
+//            String tblQuery = "create table tbl_std(id int primary key,name varchar(50),password varchar(50)"
+//                    + ",repassword varchar(50), gender varchar(50),course varchar(50))";
             
-            st.execute(tblQuery);
+//            st.execute(tblQuery);
+
+//            System.out.println("Insert operation");
+//            
+//            String insQuery = "insert into tbl_std values(2,'Ram','12345678','12345678','Female','BIM')";
+//            
+//            int res = st.executeUpdate(insQuery);
+//            if(res >=1)
+//            {
+//                System.out.println(res+ " record update");
+//            }
+//   
+
+              System.out.println("Updating data");
+              //updating name to gita and course to bca of std whose id is 2
+              String upData = "update tbl_std set name='Hari' where id =1";
+              st.execute(upData);
+
+//            System.out.println("Fetching Data");
+//            
+//            String disQuery = "select * from tbl_std";
+//            ResultSet rs = st.executeQuery(disQuery);
+//            
+//            while(rs.next()){
+//                int id = rs.getInt("id");
+//                String name = rs.getString("name");
+//                String pass = rs.getString("password");
+//                String repassword = rs.getString("repassword");
+//                String gender = rs.getString("gender");
+//                String course = rs.getString("course");
+//                
+//                System.out.println("id is " +id +" name is "+name+ " password is "+pass+" repassword is "+repassword+""
+//                    + " gender is "+gender+ " course is "+course);
+//                
+//            }
+            
         }catch(ClassNotFoundException c){
             System.out.println(c);
         }catch(SQLException e){
